12fcba91b6145c72ca3f8cac4a4a2aa3bf68a7d5
Nikhil Verma <48198080+vnikhil89@users.noreply.github.com>
2024-02-12 20:06:51 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -14,16 +14,18 @@ data "oci_core_volumes" "test_volumes" {
     availability_domain = var.ad_name
 }
 locals {
- hostname = [for ins in data.oci_core_instances.test_instances.instances[*] : ins if startswith(ins.display_name,"test-")][*]
+ hostname = [for ins in data.oci_core_instances.test_instances.instances[*] : ins if startswith(ins.display_name,"test")][*].id
 }
 
 resource "oci_core_volume_attachment" "test_volume_attachment" {
       attachment_type = "iscsi"
-      instance_id = lookup(local.hostname[0],"id")
+      for_each = toset(local.hostname)
+      instance_id = each.value
       volume_id = lookup(data.oci_core_volumes.test_volumes.volumes[1],"id")
+      is_shareable = true
 }
 
 
 output "test1" {
-  value = oci_core_volume_attachment.test_volume_attachment.ipv4
+  value = oci_core_volume_attachment.test_volume_attachment[*]
 }
